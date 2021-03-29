a64048d9e73338459ddd7ce4339f57cfe85f5533
Fabrice Notte <61163707+FabLeGris@users.noreply.github.com>
2021-03-29 15:50:10 +0200

    Add files via upload

---
+++ VIDE/_inc/Globals.class.php
@@ -0,0 +1,21 @@
+<?php
+class Globals
+{
+    private $POST;
+    private $ENV;
+    public function __construct()
+    {
+        $this->POST = $_POST;
+        $this->ENV = $_SERVER['PHP_SELF'];
+    }
+
+    public function getPOST()
+    {
+        return $this->POST;
+    }
+
+    public function getENV()
+    {
+        return $this->ENV;
+    }
+}
